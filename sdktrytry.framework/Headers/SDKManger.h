bee6cb4b8cac711c05e1a1e0fe7385f1fd50e536
zimub <1096562298@qq.com>
2021-05-17 10:54:02 +0800

    Add files via upload

---
+++ sdktrytry.framework/Headers/SDKManger.h
@@ -0,0 +1,16 @@
+//
+//  SDKManger.h
+//  originalCode
+//
+//  Created by 潘国文 on 2021/5/13.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SDKManger : NSObject
+- (void)loadImage;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ sdktrytry.framework/Headers/sdktrytry.h
@@ -0,0 +1,18 @@
+//
+//  sdktrytry.h
+//  sdktrytry
+//
+//  Created by 潘国文 on 2021/5/14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sdktrytry.
+FOUNDATION_EXPORT double sdktrytryVersionNumber;
+
+//! Project version string for sdktrytry.
+FOUNDATION_EXPORT const unsigned char sdktrytryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdktrytry/PublicHeader.h>
+
+#import <sdktrytry/SDKManger.h>SDKManger
